58fb5851ae4cedee80b8fe8fb353f710f5d42ee8
Joel Allen <joelallen720@gmail.com>
2016-10-02 19:30:08 -0700

    Clean Controller system.

--- src/Nito/Systems/Controller.cpp
+++ src/Nito/Systems/Controller.cpp
@@ -7,7 +7,7 @@
 #include <stdexcept>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include "Cpp_Utils/Fn.hpp"
+#include "Cpp_Utils/Container.hpp"
 
 #include "Nito/Components.hpp"
 #include "Nito/Graphics.hpp"
@@ -18,7 +18,7 @@ using std::pair;
 using std::vector;
 using std::runtime_error;
 using glm::vec3;
-using Cpp_Utils::accumulate;
+using Cpp_Utils::for_each;
 
 
 namespace Nito
@@ -37,21 +37,6 @@ static GLFWwindow * window;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-// Utilities
-//
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-static vec3 key_direction_accumulator(const vec3 & accumulation, const pair<int, const vec3> & key_direction)
-{
-    int key_state = glfwGetKey(window, key_direction.first);
-
-    return (key_state == GLFW_PRESS || key_state == GLFW_REPEAT)
-           ? accumulation + key_direction.second
-           : accumulation;
-}
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
 // Interface
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -66,17 +51,23 @@ void controller_update()
 {
     static const map<int, const vec3> key_directions
     {
-        { GLFW_KEY_W, vec3( 0.0f,  1.0f, 0.0f) },
-        { GLFW_KEY_S, vec3( 0.0f, -1.0f, 0.0f) },
-        { GLFW_KEY_D, vec3( 1.0f,  0.0f, 0.0f) },
-        { GLFW_KEY_A, vec3(-1.0f,  0.0f, 0.0f) },
+        { GLFW_KEY_W, vec3( 0.0f, 1.0f, 0.0f) },
+        { GLFW_KEY_S, vec3( 0.0f,-1.0f, 0.0f) },
+        { GLFW_KEY_D, vec3( 1.0f, 0.0f, 0.0f) },
+        { GLFW_KEY_A, vec3(-1.0f, 0.0f, 0.0f) },
     };
-
-    const auto direction = accumulate(vec3(), key_directions, key_direction_accumulator);
 
     for (Transform * entity_transform : entity_transforms)
     {
-        entity_transform->position += direction * 0.02f;
+        for_each(key_directions, [&](const int key, const vec3 & direction) -> void
+        {
+            int key_state = glfwGetKey(window, key);
+
+            if (key_state == GLFW_PRESS || key_state == GLFW_REPEAT)
+            {
+                entity_transform->position += direction * 0.02f;
+            }
+        });
     }
 }
 

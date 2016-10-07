8e35fbdd7ac9ae2af715cb0751ed6ba092d2da21
Joel Allen <joelallen720@gmail.com>
2016-10-07 12:53:01 -0700

    Clean Engine.cpp.

--- src/Nito/Engine.cpp
+++ src/Nito/Engine.cpp
@@ -81,25 +81,25 @@ static map<string, const Component_Handler> default_component_handlers
         "transform",
         [](const JSON & component_data) -> Component
         {
-            static const auto DEFAULT_POSITION_Z = 0.0f;
-            static const auto DEFAULT_SCALE_Z = 1.0f;
-
-            auto transform = new Transform;
             const JSON & position = component_data["position"];
             const JSON & scale = component_data["scale"];
-            transform->position = vec3(position["x"], position["y"], DEFAULT_POSITION_Z);
-            transform->scale = vec3(scale["x"], scale["y"], DEFAULT_SCALE_Z);
-            return transform;
+
+            return new Transform
+            {
+                vec3(position["x"], position["y"], 0.0f),
+                vec3(scale["x"], scale["y"], 1.0f),
+            };
         }
     },
     {
         "sprite",
         [](const JSON & component_data) -> Component
         {
-            auto sprite = new Sprite;
-            sprite->texture_path = component_data["texture_path"];
-            sprite->shader_pipeline_name = component_data["shader_pipeline_name"];
-            return sprite;
+            return new Sprite
+            {
+                component_data["texture_path"],
+                component_data["shader_pipeline_name"],
+            };
         }
     },
     {

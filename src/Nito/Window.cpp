95a570721822e088d7db0b77dc31379c0ffb96bf
Joel Allen <joelallen720@gmail.com>
2016-10-11 11:29:11 -0700

    Clean Window.cpp.

--- src/Nito/Window.cpp
+++ src/Nito/Window.cpp
@@ -62,7 +62,7 @@ void init_glfw()
 
     if (!glfwInit())
     {
-        throw runtime_error("ERROR: failed to initialize GLFW!");
+        throw runtime_error("GLFW ERROR: failed to initialize GLFW!");
     }
 }
 
@@ -104,7 +104,7 @@ GLFWwindow * create_window(const Window_Config & window_config)
 
     if (window == nullptr)
     {
-        throw runtime_error("GLFW ERROR: failed to create GLFW window!");
+        throw runtime_error("GLFW ERROR: failed to create window!");
     }
 
     windows.push_back(window);

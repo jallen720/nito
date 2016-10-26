0e82249976005d723ecaf81416cc127f45164d1d
Joel Allen <joelallen720@gmail.com>
2016-10-26 11:36:10 -0700

    Make renderer use a render order based on an entity's y position.

--- src/Nito/Systems/Renderer.cpp
+++ src/Nito/Systems/Renderer.cpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include "Cpp_Utils/Vector.hpp"
 
 #include "Nito/Components.hpp"
 #include "Nito/Graphics.hpp"
@@ -9,6 +10,9 @@
 
 using std::vector;
 using std::string;
+
+// Cpp_Utils/Vector.hpp
+using Cpp_Utils::sort;
 
 
 namespace Nito
@@ -23,6 +27,18 @@ namespace Nito
 static vector<string *> entity_render_layers;
 static vector<Sprite *> entity_sprites;
 static vector<Transform *> entity_transforms;
+static vector<int> render_order;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Utilities
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+static bool highest_y_position(const int index_a, const int index_b)
+{
+    return entity_transforms[index_a]->position.y > entity_transforms[index_b]->position.y;
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -35,17 +51,20 @@ void renderer_subscribe(const Entity entity)
     entity_render_layers.push_back((string *)get_component(entity, "render_layer"));
     entity_sprites.push_back((Sprite *)get_component(entity, "sprite"));
     entity_transforms.push_back((Transform *)get_component(entity, "transform"));
+    render_order.push_back(render_order.size());
 }
 
 
 void renderer_update()
 {
-    for (auto i = 0u; i < entity_sprites.size(); i++)
+    sort(render_order, highest_y_position);
+
+    for (const int index : render_order)
     {
         load_render_data(
-            entity_render_layers[i],
-            entity_sprites[i],
-            entity_transforms[i]);
+            entity_render_layers[index],
+            entity_sprites[index],
+            entity_transforms[index]);
     }
 }
 

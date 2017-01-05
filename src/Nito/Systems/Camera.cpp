bcf3755c30d8e6cab4b24b567b8944cc60876ae1
Joel Allen <joelallen720@gmail.com>
2017-01-05 16:45:19 -0500

    Remove support for multiple cameras.

--- src/Nito/Systems/Camera.cpp
+++ src/Nito/Systems/Camera.cpp
@@ -1,9 +1,8 @@
 #include "Nito/Systems/Camera.hpp"
 
 #include <map>
+#include <stdexcept>
 #include <glm/glm.hpp>
-#include "Cpp_Utils/Map.hpp"
-#include "Cpp_Utils/Collection.hpp"
 
 #include "Nito/Components.hpp"
 #include "Nito/Utilities.hpp"
@@ -13,15 +12,10 @@
 
 
 using std::map;
+using std::runtime_error;
 
 // glm/glm.hpp
 using glm::vec3;
-
-// Cpp_Utils/Map.hpp
-using Cpp_Utils::remove;
-
-// Cpp_Utils/Collection.hpp
-using Cpp_Utils::for_each;
 
 
 namespace Nito
@@ -30,23 +24,23 @@ namespace Nito
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-// Data Structures
-//
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-struct Entity_State
-{
-    const Camera * camera;
-    const Dimensions * dimensions;
-    const Transform * transform;
-};
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
 // Data
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-static map<Entity, Entity_State> entity_states;
+static const Camera * entity_camera;
+static const Dimensions * entity_dimensions;
+static const Transform * entity_transform;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Utilities
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+static bool entity_subscribed()
+{
+    return entity_camera != nullptr;
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -56,18 +50,22 @@ static map<Entity, Entity_State> entity_states;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void camera_subscribe(const Entity entity)
 {
-    entity_states[entity] =
+    if (entity_subscribed())
     {
-        (Camera *)get_component(entity, "camera"),
-        (Dimensions *)get_component(entity, "dimensions"),
-        (Transform *)get_component(entity, "transform"),
-    };
+        throw runtime_error("ERROR: only one entity can be subscribed to the camera system per scene!");
+    }
+
+    entity_camera = (Camera *)get_component(entity, "camera");
+    entity_dimensions = (Dimensions *)get_component(entity, "dimensions");
+    entity_transform = (Transform *)get_component(entity, "transform");
 }
 
 
-void camera_unsubscribe(const Entity entity)
+void camera_unsubscribe(const Entity /*entity*/)
 {
-    remove(entity_states, entity);
+    entity_camera = nullptr;
+    entity_dimensions = nullptr;
+    entity_transform = nullptr;
 }
 
 
@@ -75,28 +73,28 @@ void camera_update()
 {
     static const vec3 & window_size = get_window_size();
 
-    for_each(entity_states, [&](const Entity /*entity*/, Entity_State & entity_state) -> void
+    if (!entity_subscribed())
     {
-        const Camera * entity_camera = entity_state.camera;
-        const Transform * entity_transform = entity_state.transform;
-        const float entity_width = window_size.x;
-        const float entity_height = window_size.y;
+        return;
+    }
 
-        render(
-            {
+    const float entity_width = window_size.x;
+    const float entity_height = window_size.y;
+
+    render(
+        {
+            entity_width,
+            entity_height,
+            entity_camera->z_near,
+            entity_camera->z_far,
+            calculate_view_matrix(
                 entity_width,
                 entity_height,
-                entity_camera->z_near,
-                entity_camera->z_far,
-                calculate_view_matrix(
-                    entity_width,
-                    entity_height,
-                    entity_state.dimensions->origin,
-                    entity_transform->position,
-                    entity_transform->scale,
-                    entity_transform->rotation),
-            });
-    });
+                entity_dimensions->origin,
+                entity_transform->position,
+                entity_transform->scale,
+                entity_transform->rotation),
+        });
 
     cleanup_rendering();
 }

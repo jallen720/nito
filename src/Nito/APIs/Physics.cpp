c2335dbed126c1dbca7c758b6f34be468ac57d32
Joel Allen <joelallen720@gmail.com>
2017-01-17 02:37:31 -0500

    Clean Physics API.

--- src/Nito/APIs/Physics.cpp
+++ src/Nito/APIs/Physics.cpp
@@ -169,27 +169,26 @@ void physics_api_update()
                 discriminant = sqrt(discriminant);
 
                 // Either solution may be on or off the ray so need to test both t1 is always the smaller value, because
-                // BOTH discriminant and A are nonnegative.
+                // BOTH discriminant and A are non-negative.
                 float t1 = (-B - discriminant) / (2 * A);
                 float t2 = (-B + discriminant) / (2 * A);
 
-                // 3x HIT cases:
-                //          -o->             --|-->  |            |  --|->
+                // 3 HIT cases:
+                // --|-----|-->           --|-->  |          |   --|-->
                 // Impale(t1 hit,t2 hit), Poke(t1 hit,t2>1), ExitWound(t1<0, t2 hit),
 
-                // 3x MISS cases:
-                //       ->  o                     o ->              | -> |
+                // 3 MISS cases:
+                // --> |     |            |     | -->       | --> |
                 // FallShort (t1>1,t2>1), Past (t1<0,t2<0), CompletelyInside(t1<0, t2>1)
 
-                if (t1 >= 0 && t1 <= 1)
+                if (t1 >= 0.0f && t1 <= 1.0f)
                 {
                     // t1 is the intersection, and it's closer than t2 (since t1 uses -B - discriminant) Impale, Poke
                     collisions.push_back(line_entity);
                 }
-                else if (t2 >= 0 && t2 <= 1)
+                else if (t2 >= 0.0f && t2 <= 1.0f)
                 {
                     // here t1 didn't intersect so we are either started inside the sphere or completely past it
-                    // ExitWound
                     collisions.push_back(line_entity);
                 }
             }
